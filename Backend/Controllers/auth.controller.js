81688e4a245848f79c02cf239d9ae9ca9de2a54e
Rutwik <rutwik1996@gmail.com>
2024-12-03 19:49:41 +0530

    create sign in api route and updating User Model

--- Backend/Controllers/auth.controller.js
+++ Backend/Controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js"
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../Utils/errorHandler.js";
-
+import jwt from 'jsonwebtoken'
 export const signUp=async(req,res,next)=>{
 const {username,email,password}=req.body
 
@@ -26,3 +26,50 @@ try{
    next(err)
 }
 }
+
+//signIn api 
+export const signIn=async (req,res,next)=>{
+    const {email,password}=req.body
+    if(!email || !password || email=='' || password ==''){
+       return  next(errorHandler(500,'All fields are required'))
+        }
+
+
+        try{
+
+        const validUser= await User.findOne({email})
+        if(!validUser){
+            console.log(validUser);
+            
+         return next(errorHandler(404,'User not found'))
+        }
+        
+        const validPassword= bcryptjs.compareSync(password,validUser.password)
+        if(!validPassword){
+            return next(errorHandler(400,'Invalid credentials'))
+        }
+      
+        
+        const token= jwt.sign(
+            {id:validUser._id,isAdmin:validPassword.isAdmin},
+            process.env.JWT_SECRET,
+            {expiresIn:'1d'}
+            )
+
+
+            const {password:hashedPass,...rest}=validUser._doc  
+
+
+        res.status(200).cookie('access_token',token,{
+            httpOnly:true, 
+            secure:false, //https needs to be true
+            sameSite:'None', // cross
+            maxAge:1*24*60*60*1000
+
+        }).json(rest)      
+    
+    }catch(err){
+            next(err)
+        }
+
+    }
--- Backend/Route/auth.route.js
+++ Backend/Route/auth.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { signUp } from '../Controllers/auth.controller.js';
+import { signIn, signUp } from '../Controllers/auth.controller.js';
  
 
 const router = express.Router()
 
 router.post('/signup',signUp)
+router.post('/signin',signIn)
 
 export default router;
