e8371ea126f9e663ed6bb45f4a00f8e57ffbcf49
Rutwik <rutwik1996@gmail.com>
2025-02-21 19:15:46 +0530

    changes in app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import About from "./Pages/About";
+import About from "./pages/About";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./Pages/Dashboard";
